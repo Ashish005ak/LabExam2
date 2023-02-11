598c4778fb6ad11e0bd2b0359ac503045af936c3
Ashish005ak <mail.ashish005ak@gmail.com>
2023-02-11 06:45:50 +0530

    2.2

--- deletearr.c
+++ deletearr.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 #include"readprintf.c"
-void deletegiven(int x[],int *pn,int item)
+void deletegiven(int x[],int n,int item)
 {
-    int i,j;
-    for(i=0;i<*pn;i++)
+    int i,j,cnt=0;
+    for(i=0;i<n;i++)
         if(x[i]==item)
         {
-            *pn--;
+            cnt++;
             break;
         }
-    for(j=i;j<*pn;j++)
-        x[j]=x[j+1];
+    if(cnt==1)
+    {
+        n=n-1;
+        for(j=i;j<n;j++)
+            x[j]=x[j+1];
+    }
 }
 
 void main()
@@ -19,6 +23,7 @@ void main()
     scanf("%d",&n);
     readarr(a,n);
     scanf("%d",&item);
-    deletegiven(a,&n,item);
+    deletegiven(a,n,item);
+    n=n-1;
    printarr(a,n);
 }
