73d28baab98501310a7785804c8f22d09b3ff5c6
Ashish005ak <mail.ashish005ak@gmail.com>
2023-02-18 18:30:08 +0530

    pascal

--- pascalstriangle.c
+++ pascalstriangle.c
@@ -1,16 +1,23 @@
 #include<stdio.h>
 void main()
 {
-    int n,i,j,a[50][50]={1};
+    int a[50][50],i,j,n;
     scanf("%d",&n);
     for(i=0;i<=n;i++)
     {
         for(j=0;j<=i;j++)
         {
-            if(j!=0||j!=i)
+            if(j==0||j==i)
+                a[i][j]=1;
+            else
                 a[i][j]=a[i-1][j-1]+a[i-1][j];
-            printf("%d ",a[i][j]);
         }
+    }
+    for (i=0;i<=n;i++)
+    {
+        for(j=0;j<=i;j++)
+            printf("%d ",a[i][j]);
         printf("\n");
     }
+    
 }
